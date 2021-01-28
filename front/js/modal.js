e5ae3b417af9471257c994cd32200e454aec26b1
matheusmaldonadosilva <matheussilvacontat@gmail.com>
2021-01-28 12:53:08 -0300

    Atualizacao

---
+++ front/js/modal.js
@@ -0,0 +1,30 @@
+$('#exampleModal').on('show.bs.modal', function(event) {
+    var button = $(event.relatedTarget)
+    var recipient = button.data('whatever')
+    var modal = $(this)
+    modal.find('.modal-title').text('Welcome a Desenvouve ' /*+ recipient*/ )
+    modal.find('.modal-body input').val(recipient)
+})
+
+jQuery(document).ready(function() {
+    jQuery('#modal-register').on('click', function(e) {
+        jQuery('#login-modal').modal('toggle')
+        jQuery('.modal-backdrop.fade.show').hide();
+    });
+
+    jQuery('#modal-login').on('click', function(f) {
+        jQuery('#login-modal').show();
+        jQuery('.modal-backdrop.fade.show').show();
+    });
+
+    jQuery('#button-register').on('click', function(g) {
+        jQuery('#register-modal').modal('toggle')
+        jQuery('.modal-backdrop.fade.show').hide();
+    });
+
+    jQuery('#button-register').on('click', function(h) {
+        jQuery('#confirmation-modal').show();
+        jQuery('.modal-backdrop.fade.show').show();
+    });
+
+});
